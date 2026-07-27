2a5c4c3f24a58fda99826059bfe3921a347a7a5d
Tiago Almeida <thyagoeap@gmail.com>
2026-07-27 19:04:22 +0000

    change name file

---
+++ src/test/java/com/example/gestaoconvenios/GestaoConveniosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestaoconvenios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoConveniosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
