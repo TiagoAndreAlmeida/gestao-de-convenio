126ac4a8bb4396d8facd4174cf61082e67e3674b
Tiago Almeida <thyagoeap@gmail.com>
2026-08-06 19:30:28 +0000

    added entities contato and empresa conveniada

---
+++ src/main/java/com/example/gestaoconvenios/infrastructure/convenios/persistence/entity/ContatoEntity.java
@@ -0,0 +1,115 @@
+package com.example.gestaoconvenios.infrastructure.convenios.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contatos")
+public class ContatoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String cargo;
+
+    @Column(nullable = false, length = 50)
+    private String telefone;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "empresa_conveniada_id", nullable = false)
+    private EmpresaConveniadaEntity empresaConveniada;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime atualizadoEm;
+
+    protected ContatoEntity() {
+    }
+
+    public ContatoEntity(Long id, String nome, String email, String cargo, String telefone,
+            EmpresaConveniadaEntity empresaConveniada, LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cargo = cargo;
+        this.telefone = telefone;
+        this.empresaConveniada = empresaConveniada;
+        this.criadoEm = criadoEm;
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public EmpresaConveniadaEntity getEmpresaConveniada() {
+        return empresaConveniada;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmpresaConveniada(EmpresaConveniadaEntity empresaConveniada) {
+        this.empresaConveniada = empresaConveniada;
+    }
+}
---
+++ src/main/java/com/example/gestaoconvenios/infrastructure/convenios/persistence/entity/EmpresaConveniadaEntity.java
@@ -0,0 +1,111 @@
+package com.example.gestaoconvenios.infrastructure.convenios.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empresa_conveniada")
+public class EmpresaConveniadaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 250, name = "razao_social")
+    private String razaoSocial;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String cnpj;
+
+    @Column(nullable = false, length = 250)
+    private String endereco;
+
+    @Column(nullable = false)
+    private boolean ativa;
+
+    @Column(nullable = false)
+    private boolean excluida;
+
+    @Column(nullable = false, name = "criado_em", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime criadoEm;
+
+    @Column(nullable = false, name = "atualizado_em")
+    @UpdateTimestamp
+    private LocalDateTime atualizadoEm;
+
+    protected EmpresaConveniadaEntity() {
+    }
+
+    public EmpresaConveniadaEntity(long id, String razaoSocial, String cnpj, String endereco, boolean ativa,
+            boolean excluida, LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
+        this.id = id;
+        this.razaoSocial = razaoSocial;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.ativa = ativa;
+        this.excluida = excluida;
+        this.criadoEm = criadoEm;
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public boolean isExcluida() {
+        return excluida;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public void setExcluida(boolean excluida) {
+        this.excluida = excluida;
+    }
+}
