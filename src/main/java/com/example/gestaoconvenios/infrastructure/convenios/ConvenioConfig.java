db6c18f452e9832486462bb170c8d266fda7201a
Tiago Almeida <thyagoeap@gmail.com>
2026-09-04 17:09:48 +0000

    (feat): Add Bean for CadastraContatoUseCase

--- src/main/java/com/example/gestaoconvenios/infrastructure/convenios/ConvenioConfig.java
+++ src/main/java/com/example/gestaoconvenios/infrastructure/convenios/ConvenioConfig.java
@@ -3,6 +3,7 @@ package com.example.gestaoconvenios.infrastructure.convenios;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.gestaoconvenios.application.contatos.CadastraContatoUseCase;
 import com.example.gestaoconvenios.application.convenios.cadastraempresa.CadastraEmpresaConveniadaUseCase;
 import com.example.gestaoconvenios.domain.repository.ContatoRepository;
 import com.example.gestaoconvenios.domain.repository.EmpresaConveniadaRepository;
@@ -14,4 +15,8 @@ public class ConvenioConfig {
         return new CadastraEmpresaConveniadaUseCase(empresaConveniadaRepository, contatoRepository);
     }
 
+    @Bean
+    CadastraContatoUseCase cadastraContatoUseCase(ContatoRepository contatoRepository, EmpresaConveniadaRepository empresaConveniadaRepository) {
+        return new CadastraContatoUseCase(contatoRepository, empresaConveniadaRepository);
+    }
 }
