b7a9e008e6dcf80697967c1007614c26b0c1098f
Tiago Almeida <thyagoeap@gmail.com>
2026-09-11 17:15:37 +0000

    (feat): implement new router for get list of contato from empresa conveniada

--- src/main/java/com/example/gestaoconvenios/infrastructure/convenios/controller/contato/ContatoController.java
+++ src/main/java/com/example/gestaoconvenios/infrastructure/convenios/controller/contato/ContatoController.java
@@ -3,11 +3,17 @@ package com.example.gestaoconvenios.infrastructure.convenios.controller.contato;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.gestaoconvenios.application.contatos.CadastraContatoCommand;
-import com.example.gestaoconvenios.application.contatos.CadastraContatoUseCase;
+import com.example.gestaoconvenios.application.convenios.contatos.buscacontatos.BuscaContatosCommand;
+import com.example.gestaoconvenios.application.convenios.contatos.buscacontatos.BuscaContatosUseCase;
+import com.example.gestaoconvenios.application.convenios.contatos.cadastracontato.CadastraContatoCommand;
+import com.example.gestaoconvenios.application.convenios.contatos.cadastracontato.CadastraContatoUseCase;
+import com.example.gestaoconvenios.application.shared.pagination.PaginatedResult;
 import com.example.gestaoconvenios.domain.entity.convenios.Contato;
+import com.example.gestaoconvenios.infrastructure.convenios.controller.contato.request.BuscaContatosRequest;
 import com.example.gestaoconvenios.infrastructure.convenios.controller.contato.request.CadastraContatoRequest;
+import com.example.gestaoconvenios.infrastructure.convenios.controller.contato.response.BuscaContatoResponse;
 import com.example.gestaoconvenios.infrastructure.convenios.controller.contato.response.CadastraContatoResponse;
+import com.example.gestaoconvenios.infrastructure.shared.response.PaginatedResponse;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,11 +22,17 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -28,9 +40,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "Contatos", description = "Endpoints para gerenciamento de contatos de empresas conveniadas")
 public class ContatoController {
     private CadastraContatoUseCase cadastraContatoUseCase;
+    private BuscaContatosUseCase buscaContatosUseCase;
 
-    public ContatoController(CadastraContatoUseCase cadastraContatoUseCase) {
+    public ContatoController(CadastraContatoUseCase cadastraContatoUseCase, BuscaContatosUseCase buscaContatosUseCase) {
         this.cadastraContatoUseCase = cadastraContatoUseCase;
+        this.buscaContatosUseCase = buscaContatosUseCase;
     }
 
     @PostMapping("/empresas/{empresaId}/contatos")
@@ -84,5 +98,51 @@ public class ContatoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
     
+    @GetMapping("/empresas/{empresaId}/contatos")
+    @Operation(
+        summary = "Busca contatos de uma empresa conveniada",
+        description = "Retorna uma lista de contatos associados a uma empresa conveniada existente no sistema, filtrando por nome ou email.",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Lista de contatos retornada com sucesso",
+                content = @Content(schema = @Schema(implementation = BuscaContatoResponse.class))
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Empresa conveniada não encontrada",
+                content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
+    public ResponseEntity<PaginatedResponse<BuscaContatoResponse>> buscaContatos(@PathVariable Long empresaId, @Valid @ModelAttribute BuscaContatosRequest request) {
+        BuscaContatosCommand command = new BuscaContatosCommand(
+            empresaId,
+            request.search(),
+            request.page(),
+            request.size()
+        );
 
+        PaginatedResult<Contato> domainContatos = buscaContatosUseCase.execute(command);
+        PaginatedResponse<BuscaContatoResponse> response = new PaginatedResponse<BuscaContatoResponse>(
+            domainContatos.content().stream()
+                .map(contato -> new BuscaContatoResponse(
+                    contato.getId(),
+                    contato.getNome(),
+                    contato.getCargo(),
+                    contato.getEmail(),
+                    contato.getTelefone(),
+                    contato.getCriadoEm(),
+                    contato.getAtualizadoEm()
+                ))
+                .toList(),
+            domainContatos.page(),
+            domainContatos.size(),
+            domainContatos.totalElements(),
+            domainContatos.totalPages()
+        );
+        
+        return ResponseEntity.ok(response);
+    }
+    
 }
