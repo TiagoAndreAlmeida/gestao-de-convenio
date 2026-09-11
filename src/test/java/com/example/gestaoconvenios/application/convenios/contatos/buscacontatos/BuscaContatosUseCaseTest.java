da4f7fbfe6e6749ec01cbff030f1730158a3c7c5
Tiago Almeida <thyagoeap@gmail.com>
2026-09-11 20:35:36 +0000

    (fix): removing unused imports

--- src/test/java/com/example/gestaoconvenios/application/convenios/contatos/buscacontatos/BuscaContatosUseCaseTest.java
+++ src/test/java/com/example/gestaoconvenios/application/convenios/contatos/buscacontatos/BuscaContatosUseCaseTest.java
@@ -6,10 +6,8 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
